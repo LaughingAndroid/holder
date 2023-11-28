0339ba5aa7154216c1c43d99d9fc2bb6f533e7dd
huchunfeng <huchunfeng@yoocar.com>
2023-11-28 15:16:25 +0800

    feat:view binding

---
+++ holder/src/main/java/com/cf/holder/BaseBindingHolder.kt
@@ -0,0 +1,15 @@
+package com.cf.holder
+
+import android.view.View
+import android.view.ViewGroup
+import java.lang.reflect.ParameterizedType
+
+abstract class BaseBindingHolder<T, VB : View>(parent: ViewGroup? = null, layoutId: Int) :
+    BaseHolder<T>(parent, layoutId) {
+    val binding: VB by lazy(mode = LazyThreadSafetyMode.NONE) {
+        val type = javaClass.genericSuperclass
+        val clazz = (type as ParameterizedType).actualTypeArguments[1] as Class<VB>
+        val method = clazz.getMethod("bind", View::class.java)
+        method.invoke(null, itemView) as VB
+    }
+}
